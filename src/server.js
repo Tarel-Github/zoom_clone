f9d958d503c6bb91146b03ffb32f8ff031b3b1c9
[Github <[Github>
2022-11-24 00:32:05 +0900

    ^-^

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3750`);
+app.listen(3750, handleListen);
